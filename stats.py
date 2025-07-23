be17b46d666e8003bba301597dfaec42383a1cc1
goim01 <goim03@gmail.com>
2025-07-23 16:19:06 -0300

    Add function to count total characters in a book file

--- stats.py
+++ stats.py
@@ -3,5 +3,16 @@ def get_book_text(path):
         return f.read()
 
 def num_of_words(path):
-        book = get_book_text(path)
-        return len(book.split())
+        book_text = get_book_text(path)
+        return len(book_text.split())
+
+def num_of_characters(path):
+    book_text = get_book_text(path)
+    lower_book = book_text.lower()
+    count_dict = {}
+    for each_char in lower_book:
+        if each_char not in count_dict:
+            count_dict[each_char] = 1
+        else:
+            count_dict[each_char] += 1
+    return count_dict
